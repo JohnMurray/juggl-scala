d5826b1d8579943537f7e29b708e1682ac0ac512
John Murray <me@johnmurray.io>
2015-10-12 18:41:19 -0400

    whoops again

--- src/main/scala/juggl/scala/e18.scala
+++ src/main/scala/juggl/scala/e18.scala
@@ -7,8 +7,8 @@ object E18 {
   case class DbUser(name: String, age: Int, activeUser: Boolean)
 
   def getNameFromDb: Try[String] = Try { "Sue" }
-  def getAgeFromDb(name: String): Tri[Int] = Try { 32 }
-  def getActiveStatus(name: String): Tri[Boolean] = Try { true }
+  def getAgeFromDb(name: String): Try[Int] = Try { 32 }
+  def getActiveStatus(name: String): Try[Boolean] = Try { true }
 
   // map
   // flatMap
